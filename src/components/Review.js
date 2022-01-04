4268d6a8a1c56dbca65da8ad018d1866b8d2313d
dellilaaaaa <dellilapellokila@gmail.com>
2022-01-04 23:34:29 +0800

    review & wisata

--- src/components/Review.js
+++ src/components/Review.js
@@ -1,80 +1,76 @@
 import React from "react";
-import {useEffect} from 'react'
-import ReactStars from "react-rating-stars-component";
-import {FaStar} from 'react-icons/fa'
+import Cookies from "js-cookie";
 import axios from 'axios'
+import ReactStars from "react-rating-stars-component";
+
+import {useEffect} from 'react'
+import {FaStar} from 'react-icons/fa'
 import { useState } from "react";
-function Review({ reviews }) {
+import DropdownItem from "react-bootstrap/esm/DropdownItem";
+import { useParams } from "react-router-dom";
+
+function Review() {
+
   const [star, setStar] = useState();
-  
-  
-  const [deskripsi, setDeskripsi] = useState("");
+  const [deskripsi, setDeskripsi] = useState([]);
 
-  const ratingChange = (newRating) => {
-    setStar(newRating);
-  };
-
+  useEffect(() => {
+    const token = Cookies.get("lokaKota");
+    console.log(token)
+    axios.get("localhost:4000/comment?wisata=61d1986f97aee5b7d0bc5c5", {
+      headers: { Authorization: `Bearer ${token}` },
+    }).then((result) =>{
+      console.log(result.data);
+      setDeskripsi(result.data)
+    });
+  }, []);
+  const [input, setInput] = useState("")
+  // const ratingChange = (newRating) => {
+  //   setStar(newRating);
+  // };
+  const addData = () => {
+    setDeskripsi([...deskripsi, input])
+  }
   const submidReview = (e) => {
     e.preventDefault();
     if (star === null && deskripsi === "") {
       alert("masukkan review");
-    }else
-    {
-     const data = {
-      
-       review : deskripsi,
-       rating:star,
-     }
-    }
-  //   axios
-  //     .post(
-  //       'https://movie-app-teamc.herokuapp.com/api/create/review'
-        
-  //     )
-  //     .then((response) => console.log(response))
-  //     .catch((err) => err.response.message);
+    }else{
 
-  //   setStar()
-  
-  //   setDeskripsi('')
-  // }
+    }
 };
-useEffect(() => {
-  axios('https://movie-app-teamc.herokuapp.com/api/create/review')
-  .then((response) => console.log(response))
-   .catch((err) => err.response.message)
-   
-   
-}, []);
+
 return (
   <div className="review-container">
     <div className="main-input">
       <div>
        
-        <form className="form-column" onSubmit={submidReview}>
+        {/* <form className="form-column" onSubmit={submidReview}>
         <ReactStars
           classNames="bintang"
           size={30}
           activeColor="red"
           count={5}
-          onChange={ratingChange}
+          onChange={addData}
           value={star}
         />
           <input
             className="input-review"
             type="text area"
-            placeholder="Leave a review"
+            placeholder="review"
             value={deskripsi}
             onChange={(e) => setDeskripsi(e.target.value)}
           />
           <input type="submit" value="Submit" className="btn btn-review" />
-        </form>
+        </form> */}
       </div>
 
        <div className="main-review">
-         <div className="user-container">
-          {/* {reviews.length > 0
-            ? reviews.map((review, index) => ( */}
+         {deskripsi.map((item) => (
+           <h6>{item.review}</h6>,
+           console.log(item)
+         ))}
+         {/* <div className="user-container">
               <div  className="review-container">
          
                 <p className="nama-user">
@@ -82,11 +78,7 @@ return (
                 </p>
                 <p className="user-review">hallo</p>
               </div>
-            {/* )
-            )
-            : 'No one review yet'
-             } */}
-        </div>
+        </div> */}
       </div>
 
     </div>
