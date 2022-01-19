e9a7e31e7f47aa7b4a6476097d55b7107a80379e
dellilaaaaa <dellilapellokila@gmail.com>
2022-01-19 12:32:26 +0800

    contact us with emailJS

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,13 +1,14 @@
-import { Form, Row, Col, Button, Container } from "react-bootstrap";
-import { useState } from "react";
+import { Form, Row, Col, Container } from "react-bootstrap";
+import { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
 
 import "./Contact.css";
 
 function Contact() {
   let [contact, setContact] = useState({
-    fistname: "",
-    email: "",
-    comment: "",
+    user_name: "",
+    user_email: "",
+    message: "",
   });
   let handleComent = (e) => {
     const { name, value } = e.target;
@@ -17,14 +18,33 @@ function Contact() {
     });
   };
 
-  let submitMassage = (e) => {
+  const form = useRef();
+  const sendEmail = (e) => {
     e.preventDefault();
     setContact({
-      fistname: "",
-      email: "",
-      comment: "",
+      user_name: "",
+      user_email: "",
+      message: "",
     });
+
+    emailjs
+      .sendForm(
+        "loka-kota",
+        "loka_kota_tours",
+        form.current,
+        "user_57i5xcjs4eQFjHe8IixWV"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    alert("message sent successfully");
   };
+
   return (
     <div>
       <Container
@@ -36,7 +56,7 @@ function Contact() {
           borderRadius: "5px",
         }}
       >
-        <Form>
+        <form ref={form} onSubmit={sendEmail}>
           <h6
             style={{
               textAlign: "center",
@@ -50,53 +70,67 @@ function Contact() {
           <Row>
             <Col>
               <input
-                style={{ height: "38px", borderRadius: "5px" }}
+                style={{
+                  width: "26rem",
+                  height: "38px",
+                  borderRadius: "5px",
+                  padding: "20px",
+                  margin: "16px 0px",
+                }}
                 type="text"
-                placeholder="     Your name"
-                name="fistname"
+                name="user_name"
+                placeholder="Enter Username"
                 onChange={handleComent}
-                value={contact.fistname}
+                value={contact.user_name}
               />
             </Col>
             <Col>
               <input
-                style={{ height: "38px", borderRadius: "5px" }}
+                style={{
+                  width: "26rem",
+                  height: "38px",
+                  borderRadius: "5px",
+                  padding: "20px",
+                }}
                 type="email"
-                name="email"
-                placeholder="     Your email"
+                name="user_email"
+                placeholder="Enter Email"
                 onChange={handleComent}
-                value={contact.email}
+                value={contact.user_email}
               />
             </Col>
           </Row>
           <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
             <input
               style={{
-                width: "25rem",
+                width: "26rem",
                 marginTop: "20px",
                 height: "78px",
                 borderRadius: "5px",
+                padding: "20px",
               }}
               type="textarea"
-              name="comment"
-              placeholder="     Add your message"
+              name="message"
+              placeholder="Enter Message"
               onChange={handleComent}
-              value={contact.comment}
+              value={contact.message}
             />
           </Form.Group>
-          <Button
+          <button
             style={{
-              width: "25rem",
+              width: "26rem",
+              height: "40px",
+              borderRadius: "5px",
               backgroundColor: "green",
               border: "none",
               color: "black",
             }}
             type="submit"
-            onClick={submitMassage}
+            value="Send"
           >
             Send Massage
-          </Button>
-        </Form>
+          </button>
+        </form>
       </Container>
     </div>
   );
